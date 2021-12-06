85e9e5e7ff87f6c7b1fb89ae6fde342a5c24fd7b
Hamed Asadi Asl <90451086+hamed3958@users.noreply.github.com>
2021-12-07 02:25:19 +0330

    edit card slider, add inner shadow & card edit

--- js/main.js
+++ js/main.js
@@ -70,15 +70,12 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
 
 
-
-
-
-
+// card slider
 
 var container = document.getElementById('contain')
 var slider = document.getElementById('slider');
 var slides = document.getElementsByClassName('slide').length;
-var buttons = document.getElementsByClassName('bt');
+var buttons = document.getElementsByClassName('slider-btn');
 
 
 var currentPosition = 0;
@@ -90,69 +87,68 @@ var prevKeyActive = false;
 var nextKeyActive = true;
 
 window.addEventListener("load", checkWidth);
+window.addEventListener("resize", checkWidth);
 
 function checkWidth() {
-    containerWidth = container.offsetWidth;
-    setParams(containerWidth);
+  buttons[0].classList.add('inactive');
+  setParams(containerWidth);
+
 }
 
 function setParams(w) {
-    if (w < 551) {
-        slidesPerPage = 1;
-    } else {
-        if (w < 901) {
-            slidesPerPage = 2;
-        } else {
-            if (w < 1101) {
-                slidesPerPage = 3;
-            } else {
-                slidesPerPage = 4;
-            }
-        }
-    }
-    slidesCount = slides - slidesPerPage;
-    if (currentPosition > slidesCount) {
-        currentPosition -= slidesPerPage;
-    };
-    currentMargin = - currentPosition * (100 / slidesPerPage);
-    slider.style.marginLeft = currentMargin + '%';
-    if (currentPosition > 0) {
-        buttons[0].classList.remove('inactive');
-    }
-    if (currentPosition < slidesCount) {
-        buttons[1].classList.remove('inactive');
-    }
-    if (currentPosition >= slidesCount) {
-        buttons[1].classList.add('inactive');
-    }
+  if (w < 551) {
+    slidesPerPage = 1;
+  } else if (w < 901) {
+    slidesPerPage = 2;
+  } else if (w < 1101) {
+    slidesPerPage = 3;
+  } else {
+    slidesPerPage = 4;
+  }
+
+  slidesCount = slides - slidesPerPage;
+  if (currentPosition > slidesCount) {
+    currentPosition -= slidesPerPage;
+  };
+  currentMargin = - currentPosition * (100 / slidesPerPage);
+  slider.style.marginLeft = currentMargin + '%';
+  if (currentPosition > 0) {
+    buttons[0].classList.remove('inactive');
+  }
+  if (currentPosition < slidesCount) {
+    buttons[1].classList.remove('inactive');
+  }
+  if (currentPosition >= slidesCount) {
+    buttons[1].classList.add('inactive');
+  }
 }
 
 setParams();
 
 function slideRight() {
-    if (currentPosition != 0) {
-        slider.style.marginLeft = currentMargin + (100 / slidesPerPage) + '%';
-        currentMargin += (100 / slidesPerPage);
-        currentPosition--;
-    };
-    if (currentPosition === 0) {
-        buttons[0].classList.add('inactive');
-    }
-    if (currentPosition < slidesCount) {
-        buttons[1].classList.remove('inactive');
-    }
+  if (currentPosition != 0) {
+    slider.style.marginLeft = currentMargin + (100 / slidesPerPage) + '%';
+    currentMargin += (100 / slidesPerPage);
+    currentPosition--;
+  };
+  if (currentPosition === 0) {
+    buttons[0].classList.add('inactive');
+  }
+  if (currentPosition < slidesCount) {
+    buttons[1].classList.remove('inactive');
+  }
 };
 
 function slideLeft() {
-    if (currentPosition != slidesCount) {
-        slider.style.marginLeft = currentMargin - (100 / slidesPerPage) + '%';
-        currentMargin -= (100 / slidesPerPage);
-        currentPosition++;
-    };
-    if (currentPosition == slidesCount) {
-        buttons[1].classList.add('inactive');
-    }
-    if (currentPosition > 0) {
-        buttons[0].classList.remove('inactive');
-    }
+  if (currentPosition != slidesCount) {
+    slider.style.marginLeft = currentMargin - (100 / slidesPerPage) + '%';
+    currentMargin -= (100 / slidesPerPage);
+    currentPosition++;
+  };
+  if (currentPosition == slidesCount) {
+    buttons[1].classList.add('inactive');
+  }
+  if (currentPosition > 0) {
+    buttons[0].classList.remove('inactive');
+  }
 };
